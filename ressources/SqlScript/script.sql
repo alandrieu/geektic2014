9bc458f8776e707726a7b10453279e4ead30641a
alexlandrieu <alexis.landrieu@omerin.com>
2014-07-03 13:23:19 +0200

    insert script.sql

--- ressources/SqlScript/script.sql
+++ ressources/SqlScript/script.sql
@@ -24,3 +24,16 @@ PRIMARY KEY (ID_GEEK, ID_INTERET),
 FOREIGN KEY (ID_GEEK) REFERENCES GEEK(ID) ON DELETE CASCADE,
 FOREIGN KEY (ID_INTERET) REFERENCES CENTRES_INTERET(ID) ON DELETE CASCADE,
 );
+
+
+INSERT INTO "PUBLIC"."GEEK"
+("NOM", "PRENOM", "AGE", "SEXE", "MAIL", "PATHAVATAR" )
+VALUES (  'Landrieu', 'alexis', '22', 'homme', 'alexis.landrieu@google.fr', '/ressources/profil/landrieu/picture/avatar.jpeg')
+
+INSERT INTO "PUBLIC"."GEEK"
+("NOM", "PRENOM", "AGE", "SEXE", "MAIL", "PATHAVATAR" )
+VALUES (  'Mick', 'jean', '25', 'femme', 'mick.jean@google.fr', '/ressources/profil/mick/picture/avatar.jpeg')
+
+INSERT INTO "PUBLIC"."GEEK"
+("NOM", "PRENOM", "AGE", "SEXE", "MAIL", "PATHAVATAR" )
+VALUES (  'carole', 'darty', '14', 'femme', 'carole.darty@google.fr', '/ressources/profil/carole/picture/avatar.jpeg')
